da6c12237e1ab5c20d9e476e626ea443f661889a
Rodrigo Aquino <anok@wolfman.devio.us>
2010-09-13 21:27:45 -0400

    oi

--- app.rb
+++ app.rb
@@ -49,14 +49,29 @@ get '/' do
   "MAGIC!"
 end
 
-get '/add/:list/:id' do
+get '/add/:list/:quant/:id' do
   list = List.first_or_create(:name => params[:list])
   id = params[:id]
-  hdrs = {"User-Agent"=>"Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; 
-en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1", 
-"Accept-Charset"=>"utf-8", "Accept"=>"text/html"}
-  table = Hpricot(open("http://store.tcgplayer.com/product.aspx?id=" + 
-params[:id], hdrs)).search("table[@class=price_list]")
+  quant = params[:quant]
+  hdrs = {"User-Agent"=>"Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O;en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1", "Accept-Charset"=>"utf-8", 
+"Accept"=>"text/html"}
+  doc = Hpricot(open("http://store.tcgplayer.com/product.aspx?id=10257", hdrs))
+  nome = doc.search("span[@id=ctl00_cphMain_lblName").inner_html #nome do card
+  table = doc.search("table[@class=price_list]").search("tr")
+  table.each do |x|
+    x = x.search('td')
+    shop = list.shops.first_or_create(:name => x[1].inner_html)
+    card = shop.stockcards.first_or_create(:name => nome)
+    card.quantity = x[3].inner_html.to_i #quantidade
+    card.price = x[4].inner_html.to_f #preço
+    card.save
+    shop.save
+  end
+  tobuycard = list.tobuycards.first_or_create(:name => nome)
+  tobuycard.quantity = quant
+  tobuycard.save
+  list.save
+  "Card #{nome} adicionado a lista #{list.name}, #{quant} unidades. para ver a lista: <a href='/see/#{list.name}'>, ou continue adicionado coisasss ;3"
 end
 
 get '/see/:list' do
